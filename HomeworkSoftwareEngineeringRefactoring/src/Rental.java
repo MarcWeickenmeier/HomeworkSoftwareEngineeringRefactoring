b1decce2df60f171d30a6235f7e8981c4857ec2c
MarcWeickenmeier <marc@weickenmeier.eu>
2017-05-18 00:41:05 +0200

    Refactored Rental.java #2

--- HomeworkSoftwareEngineeringRefactoring/src/Rental.java
+++ HomeworkSoftwareEngineeringRefactoring/src/Rental.java
@@ -1,13 +1,17 @@
 class Rental {
+	
     private Movie movie;
     private int daysRented;
+	
     public Rental(Movie newMovie, int newDaysRented) {
         movie = newMovie;
         daysRented = newDaysRented;
     }
+	
     public int getDaysRented() {
         return daysRented;
     }
+	
     public Movie getMovie() {
         return movie;
     }
